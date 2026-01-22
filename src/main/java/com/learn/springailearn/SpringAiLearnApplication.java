46a678263d0ed8300271e70b13ef0bcaf8c84864
吕阳 <159-ly@users.noreply.5ca75618cd54>
2026-01-22 10:11:01 +0800

    init

--- src/main/java/com/learn/springailearn/SpringAiLearnApplication.java
+++ src/main/java/com/learn/springailearn/SpringAiLearnApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAiLearnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAiLearnApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiLearnApplication.class, args);
+    }
 
 }
